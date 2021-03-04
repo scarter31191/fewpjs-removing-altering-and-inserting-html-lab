7f7b3d34cd09de5e9b41eff9991022c194b7c7e7
scarter31191 <scarter31191@gmaile.com>
2021-03-03 22:33:31 -0500

    commit tree 85322ffd94590df2adcb49fbbe433d3479966a07

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here!
+// let element = document.createElement('div') 
+
+// document.body.appendChild(element) 
+
+// let ul = document.createElement('ul')
+
+// let header = document.getElementById("div#header");
+
+// element.innerHTML = 'Hello, DOM!' 
+
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+
+// element.style.backgroundColor = '#27647B'; 
+
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left'
+
+// element.appendChild(ul) 
+
+// ul.remove()
+
+main.remove()
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+let element = document.getElementsByClassName("victory");
+newHeader.innerHTML = "Steve Carter is the champion";
